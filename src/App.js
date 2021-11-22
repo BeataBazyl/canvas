fa5e50d25ffcac1153d0e03c75aa69b6427a9cc0
BeataBazyl <beata.m.bazyl@gmail.com>
2021-11-22 22:32:22 +0100

    grid of ractangle canvas

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ function App() {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
 
-    context.fillStyle = "green";
-    context.fillRect(10,10,150,100);
+    context.fillStyle = "pink";
+    // context.fillRect(100,100,400,400);
+
+    context.lineWidth = 4;
+    context.beginPath();
+    context.rect(100, 100, 400, 400);
+    // context.stroke();
+
+    context.beginPath();
+    context.arc(300, 300, 100, 0, Math.PI * 2);
+    // context.stroke();
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        let width = 60;
+        let height = 60;
+        let gap = 20;
+        let x = 100 + (width + gap) * i;
+        let y = 100 + (height + gap) * j;
+    
+        context.beginPath();
+        context.rect(x, y, width, height);
+        context.stroke();
+      }
+    }
   })
 
   return (
